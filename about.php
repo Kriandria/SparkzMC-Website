abb40b8d680760d45e54e464c6cc3936ce402e0c
Ruinsane <djleh32@gmail.com>
2014-03-19 16:44:59 -0400

    2nd commit

--- about.php
+++ about.php
@@ -45,6 +45,10 @@
 
           <div class="inner cover">
             <h1 class="cover-heading">About Us</h1>
-            <p class="lead">As mentioned word for word in the first page,<br>"We at SparkzMC offer Opensource,up to date minecraft plugins, and public test servers to try them out on!"<br>What this means is, we will offer you some of the tools that will make your adventures all the more interesting, helping you in your minecraft adventures, while helping us learn how to work in this field.<br>At the same time, we will let you test them for us to see what you like and dislike, and what happens to be broken.</p>
-            <p> There are currently 2 members working on the <b>SparkzMC</b>&trade; network project, with another member laying dormant until he has the time to work. The first of these members is <i><b>Zachary Snyder</b></i></p>
-                
+            <p>As mentioned word for word in the first page,<br>"We at SparkzMC offer Opensource,up to date minecraft plugins, and public test servers to try them out on!"</p>
+            <p>What this means is, we will offer you some of the tools that will make your adventures all the more interesting, helping you in your minecraft adventures, while helping us learn how to work in this field.</p>
+            <p class="lead">There are currently 2 members working on the <b>SparkzMC</b> network project, with another member laying dormant until he has the time to work. The first of these members is <i><b>Zachary Snyder</b></i>.</p>
+            <br>
+            <br>
+            <br>
+            <p class="lead">The second of these members is <b><i>Dylan Leh</i></b>.</p>
